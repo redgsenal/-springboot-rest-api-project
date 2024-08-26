cf9b303f5800eef49cb91ca78136bbfad63584e1
Reggie <reggie.senal@gmail.com>
2024-08-27 07:32:32 +0800

    init commit

---
+++ src/main/java/com/exam/project/jobinterview/JobinterviewApplication.java
@@ -0,0 +1,13 @@
+package com.exam.project.jobinterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobinterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobinterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exam/project/jobinterview/JobinterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.project.jobinterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobinterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
