c03f91701be29fe1bdcf902763ab2d785ccbe360
Reggie <reggie.senal@gmail.com>
2024-08-29 10:58:17 +0800

    update schedule task with application properties

--- src/main/java/com/exam/project/reservation/repository/ReservationRepository.java
+++ src/main/java/com/exam/project/reservation/repository/ReservationRepository.java
@@ -10,6 +10,7 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
     @Query(value = "select u.notify_method from reservation u where u.reservation_id = ?1", nativeQuery = true)
     String findNotifyMethodById(Long id);
 
+    // list of reservations between dates
     @Query(value = "select u.reservation_id from reservation u where u.has_notify = 0 and u.timeslot between ?1 and ?2", nativeQuery = true)
     List<Long> findReservationIDsForNotification(String startDateTime, String endDateTime);
 }
--- src/main/java/com/exam/project/reservation/task/ScheduledTasks.java
+++ src/main/java/com/exam/project/reservation/task/ScheduledTasks.java
@@ -3,6 +3,7 @@ package com.exam.project.reservation.task;
 import com.exam.project.reservation.repository.ReservationRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -19,23 +20,34 @@ public class ScheduledTasks {
 
     private static final SimpleDateFormat sdt = new SimpleDateFormat("yyyy-MM-dd HH:00:00");
 
+    @Value("${app.notification.hours}")
+    private int betweenNotificationsHours;
+
+    @Value("${app.notification.enable}")
+    private boolean enableReservationNotification;
+
     public ScheduledTasks(ReservationRepository reservationRepository){
         this.reservationRepository = reservationRepository;
     }
 
     // run notification every hour from monday to saturday cron = 0 0 */1 * * 1-6"
     // alternatively cron = "*/10 * * * * *" run every 10 seconds
+    // @Scheduled(cron = "0 0 */1 * * 1-6")
+    //@Scheduled(cron = "*/5 * * * * *")
     @Scheduled(cron = "0 0 */1 * * 1-6")
     public void sendReservationNotifications() {
+        if (!enableReservationNotification){
+            return;
+        }
         log.info(">>> get reservation data list and send notification where timeSlot is less than 4 hours from how");
         Calendar cal = Calendar.getInstance();
         String startDate = sdt.format(cal.getTime());
         // 4 hrs from now
-        cal.add(Calendar.HOUR_OF_DAY, 4);
+        cal.add(Calendar.HOUR_OF_DAY, betweenNotificationsHours);
         String endDate = sdt.format(cal.getTime());
-        log.info("start time: {}", startDate);
-        log.info("end time: {}", endDate);
+        log.info(">>>> start time: {}", startDate);
+        log.info(">>>> end time: {}", endDate);
         List<Long> reservationId = reservationRepository.findReservationIDsForNotification(startDate, endDate);
-        reservationId.forEach(id -> log.info("send notification to reservation id: {}", id));
+        reservationId.forEach(id -> log.info(">>> send notification to reservation id: {}", id));
     }
 }
